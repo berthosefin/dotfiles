b265ac6ba364d47ff6ac5b2bdeecd6fe41ca2fad
Berthose Fin <berthosefin@gmail.com>
2021-02-04 14:15:04 +0300

    add draculatheme

--- .config/polybar/blocks/scripts/powermenu.sh
+++ .config/polybar/blocks/scripts/powermenu.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
-
 dir="~/.config/polybar/blocks/scripts/rofi"
 uptime=$(uptime -p | sed -e 's/up //g')
 

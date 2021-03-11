8b123b8a4b2c54e322ae8f04fbc69dd8c4a0a8a7
Berthose Fin <berthosefin@gmail.com>
2021-03-11 10:14:30 +0300

    add polybar default theme

--- .config/polybar/default/launch.sh
+++ .config/polybar/default/launch.sh
@@ -2,7 +2,7 @@
 
 # Add this script to your wm startup file.
 
-DIR="$HOME/.config/polybar/simple"
+DIR="$HOME/.config/polybar/default"
 
 # Terminate already running bar instances
 killall -q polybar
@@ -11,4 +11,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q bar -c "$DIR"/config.ini &
+polybar -q default -c "$DIR"/config &
--- .config/polybar/simple/scripts/launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# By Birkhoff
-#
-
-rofi -modi run,drun \
-	-show drun \
-	-show-icons \
-	-theme ~/.config/polybar/fin/scripts/rofi/config.rasi
